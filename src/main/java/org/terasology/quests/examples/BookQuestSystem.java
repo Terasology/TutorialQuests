7f97205b28c19a7bde8c3066b0e588a3aef1621a
Tobias Nett <skaldarnar@googlemail.com>
2021-09-02 23:49:52 +0200

    style: update copyright and remove unused imports

--- src/main/java/org/terasology/quests/examples/BookQuestSystem.java
+++ src/main/java/org/terasology/quests/examples/BookQuestSystem.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2019 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.quests.examples;
 
@@ -23,12 +10,12 @@ import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.registry.In;
 import org.terasology.module.inventory.events.BeforeItemRemovedFromInventory;
 import org.terasology.tasks.Quest;
 import org.terasology.tasks.components.QuestComponent;
 import org.terasology.tasks.events.BeforeQuestEvent;
 import org.terasology.tasks.systems.QuestSystem;
-import org.terasology.engine.registry.In;
 
 /**
  * Handles quest attached to book entity.
--- src/main/java/org/terasology/quests/examples/QuestPointEntityProvider.java
+++ src/main/java/org/terasology/quests/examples/QuestPointEntityProvider.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.quests.examples;
 
@@ -24,10 +11,7 @@ import org.terasology.engine.entitySystem.entity.EntityStore;
 import org.terasology.engine.entitySystem.prefab.Prefab;
 import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.logic.nameTags.NameTagComponent;
-import org.terasology.gestalt.assets.management.AssetManager;
-import org.terasology.nui.Color;
 import org.terasology.engine.registry.In;
-import org.terasology.tasks.components.QuestListComponent;
 import org.terasology.engine.utilities.Assets;
 import org.terasology.engine.world.generation.EntityBuffer;
 import org.terasology.engine.world.generation.EntityProviderPlugin;
@@ -35,6 +19,9 @@ import org.terasology.engine.world.generation.Region;
 import org.terasology.engine.world.generation.facets.ElevationFacet;
 import org.terasology.engine.world.generation.facets.SurfacesFacet;
 import org.terasology.engine.world.generator.plugin.RegisterPlugin;
+import org.terasology.gestalt.assets.management.AssetManager;
+import org.terasology.nui.Color;
+import org.terasology.tasks.components.QuestListComponent;
 
 /**
  * Adds a quest point to any world generator
